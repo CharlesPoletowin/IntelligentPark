62af17f8e36d6e281db697c911cd369cde30efc1
CharlesPoletoWin <1409985968@qq.com>
2020-07-08 18:26:33 +0800

    add /api/data/all to get data we want

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from app.api.data import yp_data
 from app.api.user import yp_user
 from app.api.test import yp_test
 from app.api.tag import yp_tag
@@ -6,6 +7,7 @@ from app.api.tag import yp_tag
 
 def create_blueprint():
     bp = Blueprint('bp_api', __name__, url_prefix='/api')
+    yp_data.register(bp)
     yp_user.register(bp)
     yp_test.register(bp)
     yp_tag.register(bp)
---
+++ app/api/data.py
@@ -0,0 +1,43 @@
+# author    : Charles
+# time      ：2020/7/8  18:14 
+# file      ：getData.PY
+# project   ：apiToy
+# IDE       : PyCharm
+
+from app.libs.yellowprint import YellowPrint
+from flask import jsonify
+
+yp_data = YellowPrint('yp_data', url_prefix='/data')
+
+
+@yp_data.route('/all', methods=['GET'])
+def get_data():
+    data = {
+      "code": 200,
+      "time": "2020-05-28,14:29:31",
+      "day_of_week": 1,
+      "data": {
+        "building": 50,
+        "building_in": 30,
+        "detectors": 5000,
+        "area": 4999,
+        "usage": {
+          "week": [41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,123,44,88],
+          "month": []
+        },
+        "usage_forcast": {
+          "week": [41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,123,44,88],
+          "month": []
+        },
+        "power": {
+          "peak":2000,
+          "week": [41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,123,44,88],
+          "week_forcast": [41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,41,41,41,41,41,12,21,51,51,12,123,44,88],
+          "advise": []
+        },
+        "ratio_public":2000,
+        "ratio_company":2000,
+        "num_company":34
+      }
+    }
+    return jsonify(data)
