9c7a328135eb81f9a3d8d7b21a52e1f5868013ba
CharlesPoletoWin <1409985968@qq.com>
2020-07-22 14:49:26 +0800

    update requests header so that I could get the temperature

--- app/libs/get_temperature.py
+++ app/libs/get_temperature.py
@@ -9,13 +9,10 @@ from lxml import etree
 
 
 def get_temperature_shanghai():
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-    }
+    headers = {"User-Agent": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1 Trident/5.0;"}
     url = "https://www.baidu.com/s?wd=%E4%B8%8A%E6%B5%B7%E6%B0%94%E6%B8%A9&ie=UTF-8"
     html = requests.get(url, headers=headers)
-    html_doc = html.content.decode("utf-8")
-    selector = etree.HTML(html_doc)
+    selector = etree.HTML(html.text)
     path1 = "/html/body/div/div[3]/div[1]/div[3]/div[1]/div[1]/div[1]/a[1]/div[1]/div[2]/span[1]"
     results = selector.xpath(path1)
     return results[0].text
